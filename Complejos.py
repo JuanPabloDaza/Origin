7923083794ba12adebf1a4241b06adc854c35fe6
JuanPabloDaza <97971860+JuanPabloDaza@users.noreply.github.com>
2022-01-24 19:58:32 -0500

    Add files via upload

--- Complejos.py
+++ Complejos.py
@@ -1,3 +1,4 @@
+import math
 
 def sumacplx(a, b):
     real = a[0] + b[0]
@@ -14,12 +15,22 @@ def divcplx(a, b):
     img = ((b[0] * a[1])-(a[0] * b[1]))/((b[0]^2) + (b[1]^2))
     return (real, img)
 
+def modulocplx(a):
+    return math.sqrt((a[0])^2 + (a[1])^2)
+
+def conjugadocplx(a):
+    return (a[0], (-1*a[1]))
+
 def prettyprinting(c):
-    print(str(c[0]) + " + " + str(c[1]) + "i")
+    if c[1] > 0:
+        print(str(c[0]) + " + " + str(c[1]) + "i")
+    else:
+        print(str(c[0]) + " " + str(c[1]) + "i")
 
 
 #if __name__ == '__main__':
 prettyprinting(sumacplx((2,3), (4,7)))
 prettyprinting(multcplx((2,3), (4,7)))
-
-
+prettyprinting(divcplx((2,3), (4,7)))
+print(modulocplx((2,3)))
+prettyprinting(conjugadocplx((2,3)))
